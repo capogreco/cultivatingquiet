b63e332de3cb36b55821e0d53626210214e01806
capogreco <thomas.capogreco@gmail.com>
2023-04-10 13:09:14 +1000

    commented out websocket stuff

--- main.js
+++ main.js
@@ -12,39 +12,39 @@ let update_timer = null
 const req_handler = async req => {
    const path = new URL(req.url).pathname
 
-   const upgrade = req.headers.get ("upgrade") || ""
-   if (upgrade.toLowerCase () == "websocket") {
+   // const upgrade = req.headers.get ("upgrade") || ""
+   // if (upgrade.toLowerCase () == "websocket") {
 
-      const { socket, response } = Deno.upgradeWebSocket (req)
-      const id = uuid.v1.generate ()
+   //    const { socket, response } = Deno.upgradeWebSocket (req)
+   //    const id = uuid.v1.generate ()
 
-      socket.onopen = () => {
+   //    socket.onopen = () => {
 
-         squares.set (id, new FloatingSquare (socket))
-         console.log (`welcome, ${ id }.`)
+   //       squares.set (id, new FloatingSquare (socket))
+   //       console.log (`welcome, ${ id }.`)
 
-         const data = { 
-            'type' : `id`,
-            'body' : id,
-         }
+   //       const data = { 
+   //          'type' : `id`,
+   //          'body' : id,
+   //       }
 
-         socket.send (JSON.stringify (data))
+   //       socket.send (JSON.stringify (data))
 
-      }
+   //    }
 
-      socket.onerror = e => console.log(`socket error: ${ e.message }`)
+   //    socket.onerror = e => console.log(`socket error: ${ e.message }`)
 
-      socket.onclose = () => {
-         squares.delete (id)
-      }
+   //    socket.onclose = () => {
+   //       squares.delete (id)
+   //    }
 
-      socket.onmessage = e => {
-         const obj = JSON.parse (e.data)
-         console.log (`message recieved: ${ obj }`)
-      }
+   //    socket.onmessage = e => {
+   //       const obj = JSON.parse (e.data)
+   //       console.log (`message recieved: ${ obj }`)
+   //    }
 
-      return response
-   }
+   //    return response
+   // }
 
    let new_path = `public${ path }`
 
@@ -69,45 +69,45 @@ serve (req_handler, { port: 80 })
 
 // update ()
 
-class FloatingSquare {
-   constructor(websocket) {
-      this.ws       = websocket
-      this.speed    = 0.0001
-      this.noise3D  = openSimplexNoise3D (Math.random () * MAX)
-      this.x = Math.random () * MAX
-      this.y = Math.random () * MAX
-      this.z = Math.random () * MAX
-   }
+// class FloatingSquare {
+//    constructor(websocket) {
+//       this.ws       = websocket
+//       this.speed    = 0.0001
+//       this.noise3D  = openSimplexNoise3D (Math.random () * MAX)
+//       this.x = Math.random () * MAX
+//       this.y = Math.random () * MAX
+//       this.z = Math.random () * MAX
+//    }
 
-   send_data(t) {
-      const adj_t = t * this.speed
-      const x_pos = this.noise3D(adj_t + this.x, this.y, this.z)
-      const y_pos = this.noise3D(this.x, adj_t + this.y, this.z)
-      const z_pos = this.noise3D(this.x, this.y, adj_t + this.z)
+//    send_data(t) {
+//       const adj_t = t * this.speed
+//       const x_pos = this.noise3D(adj_t + this.x, this.y, this.z)
+//       const y_pos = this.noise3D(this.x, adj_t + this.y, this.z)
+//       const z_pos = this.noise3D(this.x, this.y, adj_t + this.z)
 
-      const data = {
-         'type' : `data`,
-         'body' : [ x_pos, y_pos, z_pos ]
-      }
+//       const data = {
+//          'type' : `data`,
+//          'body' : [ x_pos, y_pos, z_pos ]
+//       }
 
-      this.ws.send (JSON.stringify (data))
-   }
-}
+//       this.ws.send (JSON.stringify (data))
+//    }
+// }
 
 
-function checkSquares () {
-   const removals = []
-   squares.forEach (s => {
-      if (s.ws.val.readyState == 3) {
-         removals.push (s.key)
-   }
-   })
+// function checkSquares () {
+//    const removals = []
+//    squares.forEach (s => {
+//       if (s.ws.val.readyState == 3) {
+//          removals.push (s.key)
+//    }
+//    })
 
-   if (removals.length) {
-      removals.forEach (id => {
-         sockets.delete (id)
-      })
-      updateControl ()
-   }
-}
+//    if (removals.length) {
+//       removals.forEach (id => {
+//          sockets.delete (id)
+//       })
+//       updateControl ()
+//    }
+// }
 
