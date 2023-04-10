f881cff50b3aa4305809787f221a438f1bf4ae1e
capogreco <thomas.capogreco@gmail.com>
2023-04-10 12:13:23 +1000

    white moving square

---
+++ main.js
@@ -0,0 +1,116 @@
+import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
+import { serveFile } from "https://deno.land/std@0.157.0/http/file_server.ts";
+import { openSimplexNoise3D } from "https://deno.land/x/noise/mod.ts";
+import * as uuid from "https://deno.land/std@0.119.0/uuid/mod.ts";
+
+const MAX = 2 ** 24
+const period = 41.66666667
+const squares = new Map ()
+let time = Date.now ()
+let update_timer = null
+
+const req_handler = async req => {
+   const path = new URL(req.url).pathname
+
+   const upgrade = req.headers.get ("upgrade") || ""
+   if (upgrade.toLowerCase () == "websocket") {
+
+      const { socket, response } = Deno.upgradeWebSocket (req)
+      const id = uuid.v1.generate ()
+
+      socket.onopen = () => {
+
+         squares.set (id, new FloatingSquare (socket))
+         console.log (`welcome, ${ id }.`)
+
+         const data = { 
+            'type' : `id`,
+            'body' : id,
+         }
+
+         socket.send (JSON.stringify (data))
+
+      }
+
+      socket.onerror = e => console.log(`socket error: ${ e.message }`)
+
+      socket.onclose = () => {
+         squares.delete (id)
+      }
+
+      socket.onmessage = e => {
+         const obj = JSON.parse (e.data)
+         console.log (`message recieved: ${ obj }`)
+      }
+
+      return response
+   }
+
+   let new_path = `public${ path }`
+
+   if (new_path.endsWith (`/`)) {
+      new_path += `index.html`
+   }
+
+   return serveFile (req, new_path)
+}
+
+
+serve (req_handler, { port: 80 })
+
+
+
+function update () {
+   time += period
+
+   squares.forEach ((s, i) => {
+      s.send_data (time)
+   })
+
+   update_timer = setTimeout (update, period)
+}
+
+update ()
+
+class FloatingSquare {
+   constructor(websocket) {
+      this.ws       = websocket
+      this.speed    = 0.0001
+      this.noise3D  = openSimplexNoise3D (Math.random () * MAX)
+      this.x = Math.random () * MAX
+      this.y = Math.random () * MAX
+      this.z = Math.random () * MAX
+   }
+
+   send_data(t) {
+      const adj_t = t * this.speed
+      const x_pos = this.noise3D(adj_t + this.x, this.y, this.z)
+      const y_pos = this.noise3D(this.x, adj_t + this.y, this.z)
+      const z_pos = this.noise3D(this.x, this.y, adj_t + this.z)
+
+      const data = {
+         'type' : `data`,
+         'body' : [ x_pos, y_pos, z_pos ]
+      }
+
+      this.ws.send (JSON.stringify (data))
+   }
+}
+
+
+function checkSquares () {
+   const removals = []
+   squares.forEach (s => {
+      if (s.ws.val.readyState == 3) {
+         removals.push (s.key)
+   }
+   })
+
+   if (removals.length) {
+      removals.forEach (id => {
+         sockets.delete (id)
+      })
+      updateControl ()
+   }
+}
+
--- public/script.js
+++ public/script.js
@@ -2,81 +2,59 @@ document.body.style.margin   = 0
 document.body.style.overflow = `hidden`
 document.body.style.background = `black`
 
-const free_elements = []
+// const ws_address = `ws://localhost/`
+const ws_address = `wss://cultivatingquiet.space/`
 
-// call initial frame
-requestAnimationFrame (physics_engine)
+const socket = new WebSocket (ws_address)
 
-// function to move the elements around
-function physics_engine () {
-
-   // iterate through the free_elements array
-   free_elements.forEach (e => {
-
-      // if element is too far to the right
-      if (e.offsetLeft > window.innerWidth) {
-
-            // respawn it on the left
-            e.style.left = `${ -e.offsetWidth }px`
-      }
-
-      // add the elements velocity to its position
-      e.style.left = `${ e.offsetLeft + e.x_vel }px`
-   })
-   
-   // call next frame
-   requestAnimationFrame (physics_engine)
+socket.onmessage = msg => {
+   const obj = JSON.parse (msg.data)
+   console.dir (`${ obj.type } message recieved: `, obj)
+   if (obj.type == `data`) {
+      squuare.x_pos = obj.body[0]
+      squuare.y_pos = obj.body[1]
+      squuare.z_pos = obj.body[2]
+   }
 }
 
-// function to generate elements
-// accepts some text as an argument
-// assigns it to the parameter 't'
-function set_free (t) {
+const cnv = document.getElementById (`cnv`)
+cnv.width = innerWidth
+cnv.height = innerHeight
+const ctx = cnv.getContext(`2d`)
 
-   // create a div element
-   const free_div = document.createElement (`div`)
+class Squuare {
+   constructor () {
+      this.size  = Math.random () * 300
+      this.x_pos = 0
+      this.y_pos = 0
+   }
 
-   // assign the text that was passed in
-   // to the innerText property of the div
-   free_div.innerText = t 
+   draw () {
+      const m = { x: innerWidth / 2, y: innerHeight / 2 }
 
-   // format the div
-   free_div.style.fontSize   = '36px'
-   free_div.style.fontWeight = 'bold'
-   free_div.style.fontStyle  = 'italic'
-   free_div.style.color      = 'hotpink'
-   free_div.style.whiteSpace = 'nowrap'
+      let x = this.x_pos * innerWidth
+      x += innerWidth / 2
+      x -= this.size / 2
 
-   // setting .position to 'fixed' means
-   // the position is set against the viewport
-   // rather than the document
-   free_div.style.position   = 'fixed'
+      let y = this.y_pos * innerHeight
+      y += innerHeight / 2
+      y -= this.size / 2
 
-   // incorporate the div in the DOM
-   document.body.append (free_div)
-
-   // .offsetHeight is the height of the div element
-   // multiplied by how many elements are already in
-   // the free_elements array
-   const y_offset = free_div.offsetHeight * free_elements.length
-
-   // set the new element underneath the other elements
-   free_div.style.top = `${ y_offset }px`
-
-   // .offsetWidth is the width of the div
-   // start the div to the left of the screen
-   free_div.style.left = `${ -free_div.offsetWidth }px`
-
-   // we can add properties to the DOM objects
-   // simply assign to a new property
-   // and the value stays there!
-   // here we are storing a random x-velocity
-   free_div.x_vel = Math.random () * 10
-
-   // add the div to the free_elements array
-   free_elements.push (free_div)
-
-   if (y_offset < innerHeight) set_free (t)
+      console.log (x, y)
+      ctx.fillStyle = "white"
+      ctx.fillRect (x, y, this.size, this.size)
+   }
 }
 
-set_free ('Hello, Richard.')
+const squuare = new Squuare ()
+
+console.dir (squuare)
+
+function draw_frame () {
+   ctx.fillStyle = `black`
+   ctx.fillRect (0, 0, innerWidth, innerHeight)
+   squuare.draw ()
+   requestAnimationFrame (draw_frame)
+}
+
+draw_frame ()
